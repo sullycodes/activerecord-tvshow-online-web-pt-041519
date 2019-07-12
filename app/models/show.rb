29e7a63b231a187b772a2a4b0ea9b659c496a7ee
Daniel Sullivan <dansullivan19@gmail.com>
2019-07-12 16:50:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,8 +6,8 @@ class Show < ActiveRecord::Base
   
   def self.most_popular_show 
     show = self.where(":rating = ?", self.highest_rating)
-    # show = self.highest_rating
-    show.name
+    # # show = self.highest_rating
+    # show.name
   end
   
 end
